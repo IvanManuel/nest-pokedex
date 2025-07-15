0b4473e704a64e2e92344fea4dfe47b8d6397f3a
IvanManuel <mendozazivan@gmail.com>
2025-07-15 19:48:24 +0200

    first commit

---
+++ src/pokemon/pokemon.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PokemonService } from './pokemon.service';
+import { PokemonController } from './pokemon.controller';
+import { PokemonSchema } from './entities/pokemon.entity';
+
+@Module({
+  controllers: [PokemonController],
+  providers: [PokemonService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Pokemon',
+        schema: PokemonSchema,
+      },
+    ]),
+  ],
+})
+export class PokemonModule {}
